eb0b7cd0048fc150eb07ba5df044828e8ab02731
Paul Leone <35977957+pleone55@users.noreply.github.com>
2020-05-15 01:35:42 -0700

    implemented an event bus that handles the events of when a post and/or comment is generated. Added a query service to handle one request for fetching the post data and comments associated with the post

---
+++ event-bus/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios')
+
+const app = express();
+app.use(bodyParser.json());
+
+//watch for post event
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    axios.post("http://localhost:7000/events", event);
+    axios.post("http://localhost:7001/events", event);
+    axios.post("http://localhost:7002/events", event);
+
+    res.send({ status: "ok" });
+});
+
+app.listen(7005, () => {
+    console.log("Now listening on port 7005")
+});
